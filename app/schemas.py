d6c284f1d0975e86f67575a1b4ebe12840e75212
kc-saigusa <kc-saigusa@unext-hd.jp>
2024-10-03 11:20:00 +0900

    メッセージのバリデーション

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,7 @@
 # Pydanticのschema
 
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, constr, field_validator
 
 # メッセージの基本的な構造を定義
 class MessageBase(BaseModel):
@@ -9,7 +9,19 @@ class MessageBase(BaseModel):
 
 # メッセージ作成時の構造を定義。MessageBaseを継承しているためcontentを持つ。
 class MessageCreate(MessageBase):
-    pass
+    # pydanticのconstrを使って、contentが空白でないことをチェックする
+    # content: constr(min_length=1)
+
+    # メッセージのカスタムバリデーションを行う関数を定義
+    @field_validator('content')
+    # class内で定義されたメソッドは、第一引数に自分自身を受け取る必要があるが、staticmethodを使うことで第一引数を省略で
+    @staticmethod
+    def check_content(value):
+        if len(value) < 1:
+            raise ValueError('メッセージは1文字以上で送信してください')
+        if value.strip() == "":
+            raise ValueError('空白のメッセージは送信できません')
+        return value
 
 # DBから取得したメッセージを返す際の構造を定義。MessageBaseを継承しているためcontentを持つ。
 class Message(MessageBase):
